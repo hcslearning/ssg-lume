6eacc871ea45a119a673c1345d44dff1e201e756
hcslearning <learning@hardcybersoft.cl>
2025-05-12 23:19:16 -0400

    plugin sass

--- _config.ts
+++ _config.ts
@@ -1,9 +1,13 @@
 import lume from "lume/mod.ts";
+import sass from "lume/plugins/sass.ts";
 
 const site = lume({
     src: "./src",
     cssFile: "/css/estilo.css",
     jsFile: "/js/script.js",
 });
+
+site.use( sass() );
+site.add("/css/estilo.scss");
 
 export default site;
